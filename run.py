055d705f98fc28ffb32fdad0144932bd917b25e2
Gireesh1806 <124286012+Gireesh1806@users.noreply.github.com>
2024-10-19 16:19:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ app = create_app()
 socketio = SocketIO(app)  # Initialize SocketIO with your Flask app
 
 if __name__ == '__main__':
+    
     with app.app_context():
         db.create_all()  # Create database tables
-    socketio.run(app, debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
